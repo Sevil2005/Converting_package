f8c4ffd6da6b5308e2c6ace3ee5578c01947e6cf
Sevil Rasulova <sevillerasulova@gmail.com>
2020-09-12 22:26:17 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,56 @@
-n = int(input())
 var_100 = "yüz"
 var_1000 = "min"
 list1 = ["bir", "iki", "üç", "dörd", "beş",
          "altı", "yeddi", "səkkiz", "doqquz"]
 list2 = ["on", "iyirmi", "otuz", "qırx", "əlli",
-         "altmış", "yetmiş", "səksən", "doqsan"]
+         "altmış", "yetmiş", "səksən", "doxsan"]
 
 
-def teklik(a):
+def ones(a):
     for i in range(1, 10):
         if a == i:
             return list1[i-1]
 
 
-def onluq(a):
+def tens(a):
     for i in range(1, 10):
         if a == i:
             return list2[i-1]
 
 
-def yuzluk(a):
+def hundreds(a):
     for i in range(1, 10):
         if a == i:
             if i == 1:
                 return var_100
             else:
-                return teklik(i) + " " + var_100
+                return ones(i) + " " + var_100
 
 
-def minlik(a):
+def thousands(a):
     for i in range(1, 10):
         if a == i:
             if i == 1:
                 return var_1000
             else:
-                return teklik(i) + " " + var_1000
+                return ones(i) + " " + var_1000
 
 
-if len(str(n)) == 1:
-    print(teklik(n))
-elif len(str(n)) == 2:
-    print(onluq(n//10), teklik(n % 10))
-elif len(str(n)) == 3:
-    print(yuzluk(n//100), onluq((n//10) % 10), teklik(n % 10))
-elif len(str(n)) == 4:
-    print(minlik(n//1000), yuzluk((n//100) % 10),
-          onluq((n//10) % 10), teklik(n % 10))
+def convert_number_to_word(n):
+    if len(str(n)) == 1:
+        word = ones(n)
+        return word
+    elif len(str(n)) == 2:
+        word = tens(n//10) + " " + ones(n % 10)
+        return word
+    elif len(str(n)) == 3:
+        word = hundreds(n//100) + " " + tens((n//10) %
+                                             10) + " " + ones(n % 10)
+        return word
+    elif len(str(n)) == 4:
+        word = thousands(n//1000) + " " + hundreds((n//100) % 10) + \
+            " " + tens((n//10) % 10) + " " + ones(n % 10)
+        return word
+
+
+
